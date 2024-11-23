859644ddcc6bc5993a4fbb15a47cfa25d5d72dd8
blue-slushy9 <ra.cruz971@gmail.com>
2024-11-23 12:38:11 -0800

    The current version of two_sum.c works, but everything is inside of main(), so it's not the format intended by Leetcode. Will continue working on it later.

--- C/two-sum/two_sum.c
+++ C/two-sum/two_sum.c
@@ -7,31 +7,41 @@
  */
 
 // Declare function that will add the two numbers and return resulting array
-int* twoSum(int* nums, int numsSize, int target, int* returnSize); 
+//int* twoSum(int* nums, int numsSize, int target, int* returnSize); 
 
 int main() {
     // The starting array
     int nums[5] = {0, 1, 2, 42, 69};
     // The sum we are trying to reach by adding two of the elements
-    int target = 69;
+    //int target = 69;
     // Call the function that creates the final array
-    int final_array[2] = twoSum(nums, 5, 69, 2);
+    int final_array[2]; //= twoSum(&nums, 5, 69, 2);
+    
+    // Initialize final_array
+    int n=0;
+    for (int i=0; i<2; i++) {
+        final_array[i] = nums[n];
+        // Add 4 because we only want nums[0] and nums[4]
+        n+=4;
+    }
+    
     // Print the final array
     printf("Final array elements: ");
     // We want nums[0] and nums[4], so we increment by 4 and set the cap at 5
-    for (int i=0; i<5; i+=4) {
-        printf("%d ", arr[i]);
+    for (int i=0; i<2; i++) {
+        printf("%d ", final_array[i]);
     }
+
     // Free up the memory from malloc
-    free(final_array);
+    //free(final_array);
     return 0;
 }
-
+/*
 // nums is the starting array, numsSize is the size of that array,
 // target is the final sum of the two elements, returnSize is the size of the final array
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     // Allocate memory for the output array that holds our two numbers
-    int* arr = (int*) malloc(returnSize * sizeof(int));
+    int* arr = (int*) malloc(&returnSize * sizeof(int));
     if (arr == NULL) {
         printf("Memory allocation failed.\n");
     }
@@ -40,3 +50,4 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     arr[1] = nums[4];
     return arr;
 }
+*/
