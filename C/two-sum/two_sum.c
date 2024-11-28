599d8b97bb991060ca5d5ddf6e5003cd557f9fb0
blue-slushy9 <ra.cruz971@gmail.com>
2024-11-28 14:35:13 -0800

    Fleshed out two_sum.c a little more, might almost be finished.

--- C/two-sum/two_sum.c
+++ C/two-sum/two_sum.c
@@ -7,24 +7,35 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     int loop_limit = (numsSize - 1);
     // Define num1 for loop that will iterate over elements
     for (int i=0; i<loop_limit; i++) {
-        // Define variable that will be starting point for num2, we need it to be
+        // Define variable k that will be starting point for num2, we need it to be
         // one more than i because we don't want to add the same element to itself
-        int k = i + 1;
+        int k = i++;
         // Iterate over nums[], start at k as defined above; we iterate over the 
         // entire span of nums[] to cover all possible combinations
         for (int j=k; j<numsSize; j++) {
             // If num1 plus num2 equals target
             if (nums[i] + nums[j] == target) {
+                // Allocate memory for the output array that holds our two numbers;
+                // '*returnSize' dereferences the pointer to yield the value it points to
+                int* arr = (int*)malloc((*returnSize) * sizeof(int));
+                // Verify memory was successfully allocated
+                if (arr == NULL) {
+                    printf("Memory allocation failed.\n");
+                    return NULL;
+                }
                 // Initialize array that will hold the two elements
-                int arr[2] = {nums[i], nums[j]};
-                // Return the indexes of the two elements
+                arr[0] = nums[i];
+                arr[1] = nums[j];
+                // Return the array that holds the two elements
                 return arr;
+            }
+            /* Don't think we need 'else' because the for loop increases j automatically
+            // If the two elements don't add up to target value
+            } else {
+                // Increase num2 by 1
+                j++;
+            }
+            */
         }
-        else {
-            
-        }
-        
-    }
-    
-    }       
+    }   
 }
